99aece3c97d58a9b43346d514bde1a0ee1996512
Leo Uino <leo@lwh.jp>
2017-10-04 07:59:37 +0900

    binutils is now in the package catalog

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "asm")
 (define build-deps '("racket-doc" "rackunit-lib" "scribble-lib"))
-(define deps '("base" "data-lib" "git://github.com/lwhjp/racket-binutils"))
+(define deps '("base" "binutils" "data-lib"))
 (define scribblings '(("scribblings/asm.scrbl" (multi-page))))
 (define compile-omit-paths '("example/"))
